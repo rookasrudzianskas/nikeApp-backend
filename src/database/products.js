2184e81023ea121c5cba9913c56206773a574383
Rokas <rookas.rudzenskas@gmail.com>
2023-02-24 19:08:09 +0200

    Add products.js
    This file contains the functions to get all products and a single product.

---
+++ src/database/products.js
@@ -0,0 +1,15 @@
+const { ObjectId } = require('mongodb');
+const db = require('./db');
+
+const getAllProducts = async () => {
+  return await db.products.find().toArray();
+}
+
+const getProduct = async (id) => {
+  return await db.products.findOne({ _id: new ObjectId(id) });
+}
+
+module.exports = {
+  getAllProducts,
+  getProduct,
+}
