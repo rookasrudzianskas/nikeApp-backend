5b144b57982c872c1a3c2ce48f7eedfa399a9408
Rokas <rookas.rudzenskas@gmail.com>
2023-02-24 19:05:50 +0200

    feat: add mongodb
    - Add mongodb as a dependency
    - Add a db.js file to connect to the database

---
+++ src/database/db.js
@@ -0,0 +1,12 @@
+const { MongoClient } = require('mongodb');
+const uri =
+  'mongodb+srv://admin:admin@cluster0.dvnz6mb.mongodb.net/?retryWrites=true&w=majority';
+
+const client = new MongoClient(uri);
+
+const database = client.db('test');
+const products = database.collection('products');
+
+module.exports = {
+  products,
+};
