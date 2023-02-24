d4dc03f49e68e136b7a39baa49812156187f2982
Rokas <rookas.rudzenskas@gmail.com>
2023-02-24 18:08:24 +0200

    feat: rename productRouter to productRoutes
    - Rename productRouter to productRoutes
    - Update index.js to use productRoutes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const productRoutes = require('./router/productRoutes');
 
 const app = express();
 const PORT = 3000;
 
-app.use('/products', productRouter);
+app.use('/products', productRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(PORT, () => {
   console.log('API is listening on port ', PORT);
