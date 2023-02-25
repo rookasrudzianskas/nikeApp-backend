49560712552f3d5b504f7d6b9d57432cd0a961ce
Rokas <rookas.rudzenskas@gmail.com>
2023-02-25 10:36:05 +0200

    feat: add orders database
    Add a new database for orders.

---
+++ src/database/orders.js
@@ -0,0 +1,15 @@
+const db = require('./db');
+
+const getOrder = async (ref) => {
+  return await db.orders.findOne({ ref });
+};
+
+const createOrder = async (order) => {
+  const result = await db.orders.insertOne(order);
+  return { ...order, _id: result.insertedId };
+};
+
+module.exports = {
+  getOrder,
+  createOrder,
+};
