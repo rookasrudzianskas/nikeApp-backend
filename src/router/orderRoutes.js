032b176d06747c53f8721eef0fc76c6396479075
Rokas <rookas.rudzenskas@gmail.com>
2023-02-25 11:05:02 +0200

    Refactor orderRoutes.js
    - Remove unnecessary if statements
    - Use implicit returns

--- src/router/orderRoutes.js
+++ src/router/orderRoutes.js
@@ -4,21 +4,14 @@ const { createOrder, getOrder } = require('../database/orders');
 
 router.get('/:reference', async (req, res) => {
   const order = await getOrder(req.params.reference);
-
-  if (!order) {
-    res.status(404).send({ status: 'FAILED', error: 'Order not found' });
-    return;
-  }
-
+  if (!order) return res.status(404).send({ status: 'FAILED', error: 'Order not found' });
   res.send({ status: 'OK', data: order });
 });
 
 router.post('/', async (req, res) => {
   const orderData = req.body;
   orderData.ref = (Math.random() + 1).toString(36).substring(7);
-
   const newOrder = await createOrder(orderData);
-
   res.status(201).send({ status: 'OK', data: newOrder });
 });
 
