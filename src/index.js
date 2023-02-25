126b872514c1d990712bbcab7a85177540688d88
Rokas <rookas.rudzenskas@gmail.com>
2023-02-25 10:53:54 +0200

    Add body-parser to the project
    This is needed to parse the body of the POST request.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const productRoutes = require('./router/productRoutes');
 const orderRoutes = require('./router/orderRoutes');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3000;
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
--- src/router/orderRoutes.js
+++ src/router/orderRoutes.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { createOrder, getOrder } = require('../database/orders');
 
 router.get('/:reference', async (req, res) => {
-  res.send(`Get order ${req.params.reference}`);
+  const order = await getOrder(req.params.reference);
+
+  if (!order) {
+    res.status(404).send({ status: 'FAILED', error: 'Order not found' });
+    return;
+  }
+
+  res.send({ status: 'OK', data: order });
 });
 
-router.post('/', (req, res) => {
-  res.send('Creating an order');
+router.post('/', async (req, res) => {
+  const orderData = req.body;
+  orderData.ref = (Math.random() + 1).toString(36).substring(7);
+  const newOrder = await createOrder(orderData);
+
+  res.status(201).send({ status: 'OK', data: newOrder });
 });
 
 module.exports = router;
