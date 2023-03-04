713186ec35f26c2c39cc3c0f3d39a91551e5b4b1
Rokas <rookas.rudzenskas@gmail.com>
2023-03-04 11:22:06 +0200

    feat: add payment routes
    - add payment routes
    - add stripe

--- src/router/paymentRoutes.js
+++ src/router/paymentRoutes.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const stripe = require('stripe')('pk_test_51Mhe9RE3buS2eF5kcGHk0xjSxiIKh4V0BHcxFG8dlfunkOXqHZUhICZ1YrUPgYQnMApd8jI8n9eHRoH4hmCHm1i700Db9OW7SA');
+
+
+router.post('/intents', async (req, res) => {
+  try {
+
+  } catch (error) {
+    res.status(401).send({ status: "FAILED", error: error.message });
+  }
+})
 
 module.exports = router;
