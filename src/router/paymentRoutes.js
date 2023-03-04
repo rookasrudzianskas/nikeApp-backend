fa1eeb0e401d9cc85423fa72007bf7b1a6de0a11
Rokas <rookas.rudzenskas@gmail.com>
2023-03-04 11:26:25 +0200

    Add paymentIntents
    This commit adds the paymentIntents functionality to the server.
    
    The paymentIntents functionality is used to create a payment intent
    that can be used to create a payment method and charge the user.
    
    The paymentIntents functionality is used in the client to create a
    payment method and charge the user.

--- src/router/paymentRoutes.js
+++ src/router/paymentRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const stripe = require('stripe')('pk_test_51Mhe9RE3buS2eF5kcGHk0xjSxiIKh4V0BHcxFG8dlfunkOXqHZUhICZ1YrUPgYQnMApd8jI8n9eHRoH4hmCHm1i700Db9OW7SA');
+const stripe = require('stripe')('sk_test_51Mhe9RE3buS2eF5kX8XfyLymvNvo8wRQZLFbyrdRHV8jqDo1B8U7lLgFLyBl2Cu0eVbFENshCwWkzW8tzmO4Ss1l00eYq5aP2T');
 
 
 router.post('/intents', async (req, res) => {
   try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: req.body.amount,
+      currency: 'usd',
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
 
+    res.json({ paymentIntent: paymentIntent.client_secret });
   } catch (error) {
     res.status(401).send({ status: "FAILED", error: error.message });
   }
